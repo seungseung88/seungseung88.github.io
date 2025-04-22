0e9a995784ec593ba77d47aed83c43262e666376
seungseung88 <lisu5336@gmail.com>
2025-04-22 20:35:03 +0900

    delete NUXT_APP_BASE_URL
    delete config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/',
-  },
-
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/ui', '@nuxt/content'],
 });
